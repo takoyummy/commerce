698d40ab70e6aca9e64052595fdd445635859c38
takoyummy <takoyummy@naver.com>
2024-10-10 02:53:47 +0900

    feat: 잔액 관련 mock api 추가

---
+++ src/main/java/com/practice/commerce/api/dto/request/BalanceRequest.java
@@ -0,0 +1,17 @@
+package com.practice.commerce.api.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "잔액 충전 요청을 위한 데이터 전송 객체")
+public class BalanceRequest {
+	@Schema(description = "사용자 ID", example = "1", required = true)
+	private Long userId;
+	@Schema(description = "충전 또는 소진할 금액", example = "100.00", required = true)
+	private Double amount;
+}
